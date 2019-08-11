55bfb0a423b1376fc99e099becdd5366a95fd373
gpi <gpimpale29@gmail.com>
2019-08-10 22:37:45 -0700

    works

--- arduino-lasertransmitter.ino
+++ arduino-lasertransmitter.ino
@@ -1,22 +1,24 @@
 const int ledPin = 3;    // LED connected to digital pin 13
-const char* wordThing = "Yes";
+const char* string = "This is Epic\n\0\0";
 
 void setup() {
   pinMode(ledPin, OUTPUT);    // sets the digital pin as output
+  Serial.begin(9600);
 }
 
 void loop() {
-  for(int i = 0; i < strlen(wordThing); i++) {
-    unsigned char byteThing = wordThing[i];
-    out_byte(byteThing);
+  for(int i = 0; i < strlen(string)+2; i++) {
+    char byteThing = string[i];
+    for(uint8_t i = 0; i < 8; i++) {
+      // get ith bit
+      uint8_t currentBit = (byteThing >> i) & 1;
+      if(currentBit == 1) {
+       digitalWrite(ledPin, HIGH);
+      } else {
+       digitalWrite(ledPin, LOW);
+      }
+      delay(100);
+    }
+    Serial.println(byteThing);
   }
-  
-}
-
-void out_byte(unsigned char out) {
-  
-  digitalWrite(ledPin, HIGH);
-  delayMicroseconds(out);
-  digitalWrite(ledPin, LOW);
-  delayMicroseconds(256 - out);
 }
