25169ddf6006cca86154f32d91efb7cbbbffe5cb
gpi <gpimpale29@gmail.com>
2019-08-19 20:13:53 -0700

    works actually now

--- arduino-lasertransmitter.ino
+++ arduino-lasertransmitter.ino
@@ -1,24 +1,16 @@
-const int ledPin = 3;    // LED connected to digital pin 13
-const char* string = "This is Epic\n\0\0";
+#include <SoftwareSerial.h>
+
+const byte rxPin = 2;
+const byte txPin = 13; // LED is connected to pin 13
+
+SoftwareSerial mySerial (rxPin, txPin);
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);    // sets the digital pin as output
   Serial.begin(9600);
+  mySerial.begin(600);
 }
 
 void loop() {
-  for(int i = 0; i < strlen(string)+2; i++) {
-    char byteThing = string[i];
-    for(uint8_t i = 0; i < 8; i++) {
-      // get ith bit
-      uint8_t currentBit = (byteThing >> i) & 1;
-      if(currentBit == 1) {
-       digitalWrite(ledPin, HIGH);
-      } else {
-       digitalWrite(ledPin, LOW);
-      }
-      delay(100);
-    }
-    Serial.println(byteThing);
-  }
+  mySerial.println("victory is achieved");
 }
