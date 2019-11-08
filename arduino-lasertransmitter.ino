e39cca18e6d68986c1f86ebedbe9fac650734c45
pimpale <gpimpale29@gmail.com>
2019-11-07 22:48:18 -0800

    sends in packets now

--- arduino-lasertransmitter.ino
+++ arduino-lasertransmitter.ino
@@ -5,12 +5,32 @@ const byte txPin = 13; // LED is connected to pin 13
 
 SoftwareSerial mySerial (rxPin, txPin);
 
+#define BUFFERLEN 512
+int buffer[BUFFERLEN];
+int bufferIndex = 0;
 
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(600);
+  mySerial.begin(300);
+
+  while(true) {
+    while(Serial.available()) {
+      int c = Serial.read();
+      buffer[bufferIndex++] = c;
+      if(bufferIndex == BUFFERLEN || c == '\n') {
+        for(int i = 0; i < bufferIndex; i++) {
+          mySerial.write(buffer[i]);
+        }
+        bufferIndex = 0;
+      }
+    }
+  }
 }
 
 void loop() {
-  mySerial.println("victory is achieved");
+  if(random(0, 10000) == 99) {
+    mySerial.println("yeet");
+    Serial.println("yeet");
+  }
+  
 }
